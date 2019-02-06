1441cd7ddc272eb8f4c9f7093d8d6cfe0f7c1a3f
De Jong <dejong.re2@gmail.com>
2019-02-06 23:11:44 +0000

    Data Understanding and Analyzing

--- sentiment-analysis/implementation/lexicon-based.py
+++ sentiment-analysis/implementation/lexicon-based.py
@@ -7,4 +7,12 @@ Created on Sun Feb 03 12:45am 2019
 import pandas as pd
 
 # Load CSV file
-data = pd.read_csv('../datasets/amazon_unlocked_mobile_datasets.csv', header=None, na_values=['NA'], low_memory=False)
+filename = '../datasets/amazon_unlocked_mobile_datasets.csv'
+data = pd.read_csv(filename, na_values=['NA'], low_memory=False)
+
+# Data Understanding and Analyzing
+shape = data.shape
+types = data.dtypes
+print(f'No. of Rows: {shape[0]}\nNo. of Columns: {shape[1]}')
+print(f'No. of Null Values: {data.isnull().sum().sum()}')
+print(f'\nType for each attribute:\n{types}')
