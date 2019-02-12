eed1d8006338260e5843bf8f83b67621736acff2
De Jong <dejong.re2@gmail.com>
2019-02-12 15:44:11 +0000

    Spelling Check and Corrections
    Install TextBlob and Download NLTK corpora for Spelling Corrections

--- sentiment-analysis/implementation/lexicon-based.py
+++ sentiment-analysis/implementation/lexicon-based.py
@@ -5,6 +5,7 @@ Created on Sun Feb 03 12:45am 2019
 
 # Import Statement
 import pandas as pd
+from textblob import TextBlob
 
 # Load CSV file with specific column only
 filename = '../datasets/amazon_unlocked_mobile_datasets.csv'
@@ -23,7 +24,18 @@ data = data.dropna(axis=0, how='any')
 shape = data.shape
 print(f'\nAfter Cleaning:')
 print(f'No. of Rows: {shape[0]}\nNo. of Columns: {shape[1]}')
-print(f'No. of Null Values: {data.isnull().sum().sum()}')
+print(f'No. of Null Values: {data.isnull().sum().sum()}\n\n')
 
 # Remove Trailing Spaces
 data.columns = data.columns.str.strip()
+
+# Spelling Corrections
+# Install TextBlob and Download necessary NLTK corpora
+# References: https://textblob.readthedocs.io/en/dev/install.html
+print(f'Start Spelling Corrections:')
+
+for index, row in data.iterrows():
+    data.at[index, 'Reviews'] = TextBlob(row['Reviews']).correct()
+    print(f'processing...')
+
+print(f'End Spelling Corrections:\n')
