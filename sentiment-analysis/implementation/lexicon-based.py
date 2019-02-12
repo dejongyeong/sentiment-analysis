032471aa5b38c5caafe537fb4727bb3bd1cd12e1
De Jong <dejong.re2@gmail.com>
2019-02-12 14:21:25 +0000

    Data Cleaning
    Remove NULL values from CSV and use only specific columns for Sentiment Analysis

--- sentiment-analysis/implementation/lexicon-based.py
+++ sentiment-analysis/implementation/lexicon-based.py
@@ -6,13 +6,24 @@ Created on Sun Feb 03 12:45am 2019
 # Import Statement
 import pandas as pd
 
-# Load CSV file
+# Load CSV file with specific column only
 filename = '../datasets/amazon_unlocked_mobile_datasets.csv'
-data = pd.read_csv(filename, na_values=['NA'], low_memory=False)
+fields = ['Product Name', 'Brand Name', 'Reviews']
+data = pd.read_csv(filename, low_memory=False, usecols=fields)
 
 # Data Understanding and Analyzing
 shape = data.shape
 types = data.dtypes
 print(f'No. of Rows: {shape[0]}\nNo. of Columns: {shape[1]}')
+print(f'No. of Dimensions: {data.ndim}')
 print(f'No. of Null Values: {data.isnull().sum().sum()}')
-print(f'\nType for each attribute:\n{types}')
+
+# Remove Null Values
+data = data.dropna(axis=0, how='any')
+shape = data.shape
+print(f'\nAfter Cleaning:')
+print(f'No. of Rows: {shape[0]}\nNo. of Columns: {shape[1]}')
+print(f'No. of Null Values: {data.isnull().sum().sum()}')
+
+# Remove Trailing Spaces
+data.columns = data.columns.str.strip()
