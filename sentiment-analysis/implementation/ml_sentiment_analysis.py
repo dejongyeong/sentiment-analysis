55e817eb66983d46d4e1b938c4968fc03ebdd262
De Jong <dejong.re2@gmail.com>
2019-03-28 14:22:09 +0000

    comment out bag-of-word feature extraction

--- sentiment-analysis/implementation/ml_sentiment_analysis.py
+++ sentiment-analysis/implementation/ml_sentiment_analysis.py
@@ -45,110 +45,68 @@ print('split data --- end')
 print()
 
 # feature extraction
-# build bag of world feature and tfidf feature on train reviews
+# build tfidf feature on train reviews
 # min_df ignore terms that have a document frequency lower than threshold when building vocab
 # max_df ignore terms that have a document frequency higher than threshold when building vocab
-# ngram_range a tuple of lower and upper boundary of range of n-values for different n-grams to be extracted.
+# ngram_range a tuple of lower and upp \er boundary of range of n-values for different n-grams to be extracted.
 # extract 2-grams of words in addition to be 1-grams of individual words
-# reference: https://stackoverflow.com/questions/18200052/how-to-convert-ndarray-to-array
-print('feature extraction --- start')
-cv = CountVectorizer(binary=False, min_df=0.0, max_df=1.0, ngram_range=(1, 2))
-cv_train = cv.fit_transform(trainX.ravel())
-cv_test = cv.transform(testX.ravel())  # transform test review into features
-
-tv = TfidfVectorizer(use_idf=True, min_df=0.0, max_df=1.0, ngram_range=(1, 2), sublinear_tf=True)
+tv = TfidfVectorizer(use_idf=True, min_df=0.0, max_df=1.0, ngram_range=(1, 1), sublinear_tf=True)
 tv_train = tv.fit_transform(trainX.ravel())
-tv_test = cv.transform(testX.ravel())  # transform test review into features
+tv_test = tv.transform(testX.ravel())  # transform test review into features
 
-print(f'bag of word model: train: {cv_train.shape} || test: {cv_test.shape}')
 print(f'tfidf model: train: {tv_test.shape} || test: {tv_test.shape}')
 print('feature extraction --- end')
+
+feature = tv.get_feature_names()[0:100]
 
 print()
 
 print('prediction --- start')
 # predict
-# multinomial naive bayes predict on bow
+# multinomial naive bayes predict on tfidf
 mnb = MultinomialNB()
-mnb.fit(cv_train, trainY)  # build model
-cv_pred = mnb.predict(cv_test)  # predict using model
+mnb.fit(tv_train, trainY)  # build model
+tv_pred = mnb.predict(tv_test)  # predict using model
 print('prediction --- end')
-
-# evaluation
-# reference: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.precision_score.html#sklearn.metrics.precision_score
-print('\nModel Evaluation with Bag of Word:')
-cv_accuracy = np.round(mt.accuracy_score(testY, cv_pred), 2)
-cv_precision = np.round(mt.precision_score(testY, cv_pred, average='macro'), 2)
-cv_recall = np.round(mt.recall_score(testY, cv_pred, average='macro'), 2)
-cv_f1 = np.round(mt.f1_score(testY, cv_pred, average='macro'), 2)
-
-cv_metrics = np.array([cv_accuracy, cv_precision, cv_recall, cv_f1])
-cv_metrics = pd.DataFrame([cv_metrics], columns=['accuracy', 'precision', 'recall', 'f1'], index=['bag of word'])
-print('Performance Metrics:')
-print(tabulate(cv_metrics, headers='keys', tablefmt='github'))
 
 print()
 
-# predict
-# multinomial naive bayes predict on tfidf
-mnb.fit(tv_train, trainY)  # build model
-tv_pred = mnb.predict(tv_test)  # predict using model
-
 # evaluation
-print('\nModel Evaluation with TF-IDF:')
+# reference: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.precision_score.html#sklearn.metrics.precision_score
+print('\nModel Evaluation:')
 tv_accuracy = np.round(mt.accuracy_score(testY, tv_pred), 2)
 tv_precision = np.round(mt.precision_score(testY, tv_pred, average='macro'), 2)
 tv_recall = np.round(mt.recall_score(testY, tv_pred, average='macro'), 2)
 tv_f1 = np.round(mt.f1_score(testY, tv_pred, average='macro'), 2)
 
 tv_metrics = np.array([tv_accuracy, tv_precision, tv_recall, tv_f1])
-tv_metrics = pd.DataFrame([tv_metrics], columns=['accuracy', 'precision', 'recall', 'f1'], index=['tfidf'])
+tv_metrics = pd.DataFrame([tv_metrics], columns=['accuracy', 'precision', 'recall', 'f1'], index=['metrics'])
 print('Performance Metrics:')
 print(tabulate(tv_metrics, headers='keys', tablefmt='github'))
 
-# compare evaluation with different feature extraction method with graph
-compare = pd.concat([cv_metrics, tv_metrics])  # concat two dataframs
+# visualization
 fig = plt.figure()
-ax = compare.plot.bar()
-plt.title('MultinomialNB Performance Evaluation Comparison of BOW and TF-IDF\n')
+ax = tv_metrics.plot.bar()
+plt.title('MultinomialNB Performance Evaluation\n')
 plt.ylabel('result')
-plt.xlabel('feature extraction method')
+plt.xlabel('model evualtion')
 plt.xticks(rotation=-360)  # rotate x labels
 plt.ylim([0.1, 1.0])
 plt.show()
 
-print('\nConfusion Matrix of MultinomialNB with Bag of Word Feature Extraction:\n')
+print('\nConfusion Matrix of MultinomialNB:\n')
 
 # display and plot confusion matrix of bag-of-word
 labels = ['positive', 'negative', 'neutral']
-cv_cm = mt.confusion_matrix(testY, cv_pred, labels=labels)
+cv_cm = mt.confusion_matrix(testY, tv_pred, labels=labels)
 
 # plot
 # references: https://stackoverflow.com/questions/19233771/sklearn-plot-confusion-matrix-with-labels/48018785
-fig = plt.figure()
-ax = fig.add_subplot(111)
-plt.title('Confusion Matrix of MultinomialNB with Bag of Word Feature Extraction\n')
-fig.colorbar(ax.matshow(cv_cm))
-ax.set_xticklabels([''] + labels)
-ax.set_yticklabels([''] + labels)
-plt.xlabel('predicted')
-plt.ylabel('true')
-plt.show()
-
-# display in table format
-level = [len(labels)*[0], list(range(len(labels)))]
-cv_cmf = pd.DataFrame(data=cv_cm,
-                      columns=pd.MultiIndex(levels=[['predicted:'], labels], labels=level),
-                      index=pd.MultiIndex(levels=[['actual:'], labels], labels=level))
-print(cv_cmf)
-
-print('\nConfusion Matrix of MultinomialNB with TF-IDF Feature Extraction:\n')
-
-# display and plot confusion matrix of tf-idf
+# display and plot confusion matrix
 tv_cm = mt.confusion_matrix(testY, tv_pred, labels=labels)
 fig = plt.figure()
 ax = fig.add_subplot(111)
-plt.title('Confusion Matrix of MultinomialNB with TF-IDF Feature Extraction\n')
+plt.title('Confusion Matrix of MultinomialNB\n')
 fig.colorbar(ax.matshow(tv_cm))
 ax.set_xticklabels([''] + labels)
 ax.set_yticklabels([''] + labels)
@@ -163,18 +121,67 @@ tv_cmf = pd.DataFrame(data=tv_cm,
                       index=pd.MultiIndex(levels=[['actual:'], labels], labels=level))
 print(tv_cmf)
 
-print('\nConfusion Matrix of MultinomialNB with Bag of Word Feature Extraction:\n')
-
-# classification report for bag-of-word
-cv_report = mt.classification_report(testY, cv_pred, labels=labels)
-print(cv_report)
-
 print('\nClassification Report of MultinomialNB with TF-IDF Feature Extraction:\n')
 
 # classification report for tf-idf
-tv_report = mt.classification_report(testY, cv_pred, labels=labels)
+tv_report = mt.classification_report(testY, tv_pred, labels=labels)
 print(tv_report)
 
 print()
 
 # end of ml sentiment analysis
+
+"""
+Appendix
+"""
+# # feature extraction
+# # build bag of world feature and tfidf feature on train reviews
+# # reference: https://stackoverflow.com/questions/18200052/how-to-convert-ndarray-to-array
+# print('feature extraction --- start')
+# cv = CountVectorizer(binary=False, min_df=0.0, max_df=1.0, ngram_range=(1, 2))
+# cv_train = cv.fit_transform(trainX.ravel())
+# cv_test = cv.transform(testX.ravel())  # transform test review into features
+
+# # multinomial naive bayes predict on bow
+# mnb = MultinomialNB()
+# mnb.fit(tv_train, trainY)  # build model
+# tv_pred = mnb.predict(tv_test)  # predict using model
+# print('prediction --- end')
+
+# # prediction
+# print('\nModel Evaluation with Bag of Word:')
+# cv_accuracy = np.round(mt.accuracy_score(testY, cv_pred), 2)
+# cv_precision = np.round(mt.precision_score(testY, cv_pred, average='macro'), 2)
+# cv_recall = np.round(mt.recall_score(testY, cv_pred, average='macro'), 2)
+# cv_f1 = np.round(mt.f1_score(testY, cv_pred, average='macro'), 2)
+#
+# cv_metrics = np.array([cv_accuracy, cv_precision, cv_recall, cv_f1])
+# cv_metrics = pd.DataFrame([cv_metrics], columns=['accuracy', 'precision', 'recall', 'f1'], index=['bag of word'])
+# print('Performance Metrics:')
+# print(tabulate(cv_metrics, headers='keys', tablefmt='github'))
+
+# # display and plot confusion matrix of bag-of-word
+# labels = ['positive', 'negative', 'neutral']
+# cv_cm = mt.confusion_matrix(testY, cv_pred, labels=labels)
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# plt.title('Confusion Matrix of MultinomialNB with Bag of Word Feature Extraction\n')
+# fig.colorbar(ax.matshow(cv_cm))
+# ax.set_xticklabels([''] + labels)
+# ax.set_yticklabels([''] + labels)
+# plt.xlabel('predicted')
+# plt.ylabel('true')
+# plt.show()
+#
+# # display in table format
+# level = [len(labels)*[0], list(range(len(labels)))]
+# cv_cmf = pd.DataFrame(data=cv_cm,
+#                       columns=pd.MultiIndex(levels=[['predicted:'], labels], labels=level),
+#                       index=pd.MultiIndex(levels=[['actual:'], labels], labels=level))
+# print(cv_cmf)
+
+# print('\nConfusion Matrix of MultinomialNB with Bag of Word Feature Extraction:\n')
+#
+# # classification report for bag-of-word
+# cv_report = mt.classification_report(testY, cv_pred, labels=labels)
+# print(cv_report)
